9c3b19d93b87bdf1b03fb074cc96fca40e4e058c
Gautham Krishna <gauthamkrishnax@gmail.com>
2021-07-14 13:24:12 +0530

    clear all fresh start

---
+++ site/src/App.js
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div>
+        <h2>New Tabs That display time and date in a modern layout</h2>
+        <hr />
+        <div>
+          <button>Get it on Edge</button>
+          <button>Source Code</button>
+        </div>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
